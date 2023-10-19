3eafeff07508ca3b5fa8a5225385c195765d75a0
Musab Sakhreyah <mosabkhaled1998@gmail.com>
2023-10-20 01:41:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+
+function submitData(userName, eMail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: eMail
+        }),
+    })
+        .then(resp => resp.json())
+        .then(obj => document.body.append(obj.id))
+        .catch(error => document.body.append(error))
+}
+
